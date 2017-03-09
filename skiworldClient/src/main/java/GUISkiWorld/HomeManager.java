9552aad67f1ba7d1da1c42f162726c8f7ed36ba0
Iheb <Iheb@DESKTOP-1P8BS28>
2017-03-09 19:07:21 +0100

    affich

--- skiworldClient/src/main/java/GUISkiWorld/HomeManager.java
+++ skiworldClient/src/main/java/GUISkiWorld/HomeManager.java
@@ -2,15 +2,29 @@ package GUISkiWorld;
 
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
 
+import javax.ejb.Startup;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
@@ -19,6 +33,9 @@ import javax.swing.UIManager;
 
 import contracts.HotelCrudEJBRemote;
 import entities.Hotel;
+import junit.framework.Test;
+import skiworldClient.test;
+
 import javax.swing.ImageIcon;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
@@ -27,12 +44,33 @@ import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
+
+import javax.swing.ListSelectionModel;
+import javax.swing.JTextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class HomeManager {
 
 	private JFrame ManagerGUI;
 	private JTextField textField_i;
-	private JTextField textField_1;
+	private JTextField i_imagepath;
+	private String image;
+	private JTable table;
+	public DefaultTableModel tableModel;
+	test t = new test();
 
 	/**
 	 * Launch the application.
@@ -52,113 +90,164 @@ public class HomeManager {
 
 	/**
 	 * Create the application.
-	 * @throws NamingException 
+	 * 
+	 * @throws NamingException
 	 */
 	public HomeManager() throws NamingException {
 		initialize();
-		
 	}
 
 	/**
 	 * Initialize the contents of the frame.
-	 * @throws NamingException 
+	 * 
+	 * @throws NamingException
 	 */
 	private void initialize() throws NamingException {
-		try { 
-            UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
-
+		try {
+			UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
 			
-//            UIManager.setLookAndFeel(
-//                ch.randelshofer.quaqua.QuaquaManager.getLookAndFeel()  );
-       // set UI manager properties here that affect Quaqua
-       } catch (Exception e) {
-           // take an appropriate action here
-       }
-		
-		InitialContext ctx = new InitialContext();
-		HotelCrudEJBRemote proxy_hotel=(HotelCrudEJBRemote) ctx.lookup("/SkiWorld-ear/SkiWorld-ejb/HotelCrudEJB!contracts.HotelCrudEJBRemote");
 
-		
-		
+			// UIManager.setLookAndFeel(
+			// ch.randelshofer.quaqua.QuaquaManager.getLookAndFeel() );
+			// set UI manager properties here that affect Quaqua
+		} catch (Exception e) {
+			// take an appropriate action here
+		}
+
+		// InitialContext ctx = new InitialContext();
+		// HotelCrudEJBRemote proxy_hotel=(HotelCrudEJBRemote)
+		// ctx.lookup("/SkiWorld-ear/SkiWorld-ejb/HotelCrudEJB!contracts.HotelCrudEJBRemote");
 		
 		ManagerGUI = new JFrame();
 		ManagerGUI.setBounds(100, 100, 784, 459);
 		ManagerGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ManagerGUI.getContentPane().setLayout(null);
 		ManagerGUI.getContentPane().setLayout(null);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(0, 76, 768, 344);
 		ManagerGUI.getContentPane().add(tabbedPane);
-		
+
 		JPanel panel_i = new JPanel();
 		tabbedPane.addTab("Manage Hotels", null, panel_i, null);
-		
+
 		JLabel lblHotelName_i = new JLabel("Hotel  name :");
-		lblHotelName_i.setBounds(30, 29, 99, 25);
+		lblHotelName_i.setBounds(10, 27, 99, 25);
 		lblHotelName_i.setFont(new Font("Source Sans Pro", Font.PLAIN, 16));
-		
+
+		JTextArea i_textArea = new JTextArea();
+		i_textArea.setBounds(139, 82, 204, 69);
+		panel_i.add(i_textArea);
+
 		textField_i = new JTextField();
 		textField_i.setBounds(139, 31, 149, 20);
 		textField_i.setColumns(10);
-		
+
 		JLabel lblDescription = new JLabel("Description :");
-		lblDescription.setBounds(30, 81, 99, 25);
+		lblDescription.setBounds(10, 82, 99, 25);
 		lblDescription.setFont(new Font("Source Sans Pro", Font.PLAIN, 16));
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(139, 83, 259, 64);
-		textField_1.setColumns(10);
-		
+
 		JLabel lblCapacity = new JLabel("Capacity :");
-		lblCapacity.setBounds(30, 176, 99, 25);
+		lblCapacity.setBounds(10, 174, 99, 25);
 		lblCapacity.setFont(new Font("Source Sans Pro", Font.PLAIN, 16));
-		
+
 		JSpinner spinner = new JSpinner();
 		spinner.setBounds(139, 178, 123, 20);
-		
-		JLabel lblRating = new JLabel("Rating :");
+
+		JLabel lblRating = new JLabel("Image :");
 		lblRating.setBounds(30, 226, 99, 25);
 		lblRating.setFont(new Font("Source Sans Pro", Font.PLAIN, 16));
-		
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setBounds(139, 228, 123, 20);
-		
+
 		JButton btnNewButton = new JButton("Confirm");
 		btnNewButton.setBounds(93, 288, 135, 23);
 		btnNewButton.setFont(new Font("Source Sans Pro", Font.PLAIN, 16));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				Hotel hotel = new Hotel();
-				hotel.setDescription(textField_1.getText());
-				hotel.setName(textField_1.getText());
-				hotel.setCapacity((Integer)spinner.getValue());
-				hotel.setRating(Float.parseFloat(spinner_1.getValue().toString()));
-				
-				proxy_hotel.addHotel(hotel);
-				
+				InitialContext ctx;
+				try {
+					ctx = new InitialContext();
+					HotelCrudEJBRemote proxy_hotel = (HotelCrudEJBRemote) ctx
+							.lookup("/SkiWorld-ear/SkiWorld-ejb/HotelCrudEJB!contracts.HotelCrudEJBRemote");
+
+					Hotel hotel = new Hotel();
+					hotel.setDescription(i_textArea.getText());
+					hotel.setName(textField_i.getText());
+					hotel.setCapacity((Integer) spinner.getValue());
+					Path path = Paths.get(image);
+					byte[] data = Files.readAllBytes(path);
+					hotel.setImage(new SerialBlob(data));
+
+					proxy_hotel.addHotel(hotel);
+					table.setModel(t.all());
+				} catch (NamingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SerialException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
 			}
 		});
 		panel_i.setLayout(null);
 		panel_i.add(lblHotelName_i);
 		panel_i.add(textField_i);
 		panel_i.add(lblDescription);
-		panel_i.add(textField_1);
 		panel_i.add(lblCapacity);
 		panel_i.add(spinner);
 		panel_i.add(lblRating);
-		panel_i.add(spinner_1);
 		panel_i.add(btnNewButton);
+
+		JButton i_chooseimage = new JButton("choose image");
+		i_chooseimage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JFileChooser ch = new JFileChooser();
+				ch.showOpenDialog(null);
+				File f = ch.getSelectedFile();
+				image = f.getAbsolutePath();
+
+				i_imagepath.setText(image);
+			}
+		});
+		i_chooseimage.setBounds(139, 229, 113, 23);
+		panel_i.add(i_chooseimage);
+
+		i_imagepath = new JTextField();
+		i_imagepath.setBounds(262, 230, 148, 20);
+		panel_i.add(i_imagepath);
+		i_imagepath.setColumns(10);
+
+		JScrollPane scrollPane = new JScrollPane();
 		
+		scrollPane.setBounds(424, 11, 329, 165);
+		panel_i.add(scrollPane);
+
+		table = new JTable();
+		
+
+		table.setModel(t.all());
+		scrollPane.setViewportView(table);
+
 		JLabel lblNewLabel_1 = new JLabel("New label");
 		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Iheb\\Desktop\\15755113534_f75fd1636c_k.jpg"));
 		lblNewLabel_1.setBounds(0, 0, 763, 316);
 		panel_i.add(lblNewLabel_1);
-		
+		JPanel panel = new JPanel();
+
 		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Iheb\\Desktop\\Ski Freestyle Wallpaper High Definition 61942 5975 Wallpaper  Cool ....jpg"));
-		
+		lblNewLabel.setIcon(new ImageIcon(
+				"C:\\Users\\Iheb\\Desktop\\Ski Freestyle Wallpaper High Definition 61942 5975 Wallpaper  Cool ....jpg"));
+
 		lblNewLabel.setBounds(0, 0, 768, 398);
 		ManagerGUI.getContentPane().add(lblNewLabel);
 	}
--- skiworldClient/src/main/java/skiworldClient/test.java
+++ skiworldClient/src/main/java/skiworldClient/test.java
@@ -1,9 +1,41 @@
 package skiworldClient;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.swing.table.DefaultTableModel;
+
+import contracts.HotelCrudEJBRemote;
+
 public class test {
+	
  
-	int id1;
-	String a = " iheb";
-	String inaahahaha="iheb2";
+	public DefaultTableModel all() {
+		InitialContext ctx;
+
+		DefaultTableModel tableModel = null;
+		try {
+			ctx = new InitialContext();
+
+			HotelCrudEJBRemote proxy_hotel = (HotelCrudEJBRemote) ctx
+					.lookup("/SkiWorld-ear/SkiWorld-ejb/HotelCrudEJB!contracts.HotelCrudEJBRemote");
+
+			String col[] = { "Name", "Descritption", "Rating", "Capacity" };
+			tableModel = new DefaultTableModel(col, 0);
+			for (int i = 0; i < proxy_hotel.findAllHotels().size(); i++) {
+				String Name = proxy_hotel.findAllHotels().get(i).getName();
+				String Description = proxy_hotel.findAllHotels().get(i).getDescription();
+				float Rating = proxy_hotel.findAllHotels().get(i).getRating();
+				int Capacity = proxy_hotel.findAllHotels().get(i).getCapacity();
+
+				Object[] data = { Name, Description, Rating, Capacity };
+				tableModel.addRow(data);
+			}
+		} catch (NamingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return tableModel;
+
+	}
 
 }
